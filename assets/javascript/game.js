70f62f36b53afd7ed9f1555c2c341d9239153da5
Frank Capraro <Frank@Franks-MacBook-Pro.local>
2016-05-24 18:26:34 -0400

    updated what I need to get my basic wireframe off the ground

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+// Create objects for Hero & 3 villians/enemies.  Will Adjust attributes later on. 
+var hero = {healthPoints:'100' , attackPower: '6', counterAttackPower:'10'};
+var villian1 = {healthPoints:'100' , attackPower: '10', counterAttackPower:'10'};
+var villian2 = {healthPoints:'120' , attackPower: '15', counterAttackPower:'10'};
+var villian3 = {healthPoints:'150' , attackPower: '17', counterAttackPower:'10'};
+
+
+
